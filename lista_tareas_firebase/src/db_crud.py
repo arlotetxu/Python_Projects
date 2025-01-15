00960bae4069dae8b4bc3ce80c431082f03e25e8
ArLoTeTxU <arlotetxu@pm.me>
2025-01-15 18:55:38 +0100

    Applying the get_valid_input function in all CRUD functions

--- lista_tareas_firebase/src/db_crud.py
+++ lista_tareas_firebase/src/db_crud.py
@@ -75,16 +75,15 @@ def show_tasks(collection):
     - Retorno:
         - No
     """
-    while   True:
-        filter_ind = input(Fore.BLUE + """Elija el tipo de filtro:
-            \t0 - Todas las tareas.
-            \t1 - Filtrar por estado.
-            \t2 - Filtrar por prioridad.\n""" + Style.RESET_ALL)
 
-        if  not filter_ind.isdigit() or not 0 <= int(filter_ind) <= 2:
-            print(Back.RED + "Selección no válida. Inténtelo de nuevo\n" + Style.RESET_ALL)
-            continue
-        break
+    # Solicitamos tipo de filtro y comprobamos con la funcion get_valid_input() en utils.py
+    filter_ind = get_valid_input(
+        Fore.BLUE + """Elija el tipo de filtro:
+             \t0 - Todas las tareas.
+             \t1 - Filtrar por estado.
+             \t2 - Filtrar por prioridad.\n""" + Style.RESET_ALL,
+             lambda x: x.isdigit() and 0 <= int(x) <= 2,
+             "Selección no válida. Inténtelo de nuevo\n")
     if  int(filter_ind) == 0:
         show_all_db_info(collection)
 
@@ -95,26 +94,22 @@ def show_tasks(collection):
         print("\n")
         for index, state in enumerate(states):
             print(Fore.YELLOW + f"Índice: {index} - {state}" + Style.RESET_ALL)
-        while   True:
-            index = input("\nSeleccione el índice del valor a filtrar: ")
-            # comprobamos
-            if  not index.isdigit() or not 0 <= int(index) <= len(states) - 1:
-                print(Back.RED + "Índice incorrecto. Inténtelo de nuevo.\n" + Style.RESET_ALL)
-                continue
-            break
+
+        # Solicitamos valor para filtrar y comprobamos con la funcion get_valid_input() en utils.py
+        index = get_valid_input("\nSeleccione el índice del valor a filtrar: ",
+            lambda x: x.isdigit() and 0 <= int(x) <= len(states) - 1,
+            "Índice incorrecto. Inténtelo de nuevo.\n")
         show_filtered_tasks(collection, 'Estado', states[int(index)])
 
     elif  int(filter_ind) == 2:
         #obtenemos los posibles valores
         _, prior = get_field_values(collection, 'Prioridad')
         print("\n")
-        while   True:
-            prior_s = input("\nSeleccione la prioridad a filtrar (1 - 5): ")
-            # comprobamos
-            if  not prior_s.isdigit() or not 1 <= int(prior_s) <= 5:
-                print(Back.RED + "Prioridad incorrecta. Inténtelo de nuevo.\n" + Style.RESET_ALL)
-                continue
-            break
+
+        # Solicitamos prioridad y comprobamos con la funcion get_valid_input() en utils.py
+        prior_s = get_valid_input("Seleccione la prioridad a filtrar (1 - 5): ",
+            lambda x: x.isdigit() and 1 <= int(x) <= 5,
+            "Prioridad incorrecta. Inténtelo de nuevo.\n")
         show_filtered_tasks(collection, 'Prioridad', prior_s)
 
 
@@ -128,25 +123,25 @@ def update_task(collection):
         - No
     """
     field_list = get_collection_fields(collection)
+    full_ids, _ = get_field_values(collection, 'Tarea') #Lista con los IDs de la colección
     while   True:
         print("\n")
         # Mostramos toda la info de la colección
         show_all_db_info(collection)
-        id = input("\nSeleccione el ID de la tarea que desea modificar: ")
-        # Comprobamos
-        full_ids, _ = get_field_values(collection, 'Tarea') #Lista con los IDs de la colección
-        if  not id.isdigit() or not id in full_ids:
-            print(Back.RED + "El ID seleccionado no es válido. Inténtelo de nuevo." + Style.RESET_ALL)
-            continue
+
+        # Solicitamos ID de la tarea y comprobamos con la funcion get_valid_input() en utils.py
+        id = get_valid_input("\nSeleccione el ID de la tarea que desea modificar: ",
+            lambda x: x.isdigit() and x in full_ids,
+            "El ID seleccionado no es válido. Inténtelo de nuevo.")
 
         # Mostramos los campos del documente
         for index, field in enumerate(field_list):
             print(Fore.YELLOW + f"{index} - {field}" + Style.RESET_ALL)
-        field_chosen = input("Seleccione el índice del campo que desea modificar: ")
-        # Comprobamos
-        if  not field_chosen.isdigit() or not 0 <= int(field_chosen) <= len(field_list) - 1:
-            print(Back.RED + "Selección no válida. Inténtelo de nuevo." + Style.RESET_ALL)
-            continue
+
+        # Solicitamos indice del campo a modificar y comprobamos con la funcion get_valid_input() en utils.py
+        field_chosen = get_valid_input("\nSeleccione el índice del campo que desea modificar: ",
+            lambda x: x.isdigit() and 0 <= int(x) <= len(field_list) - 1,
+            "Selección no válida. Inténtelo de nuevo.")
         field_name = field_list[int(field_chosen)]
 
         # Solicitamos el cambio
@@ -180,16 +175,14 @@ def delete_task(collection):
     - Retorno:
         - No
     """
+    full_ids, _ = get_field_values(collection, 'Tarea') #Lista con los IDs de la colección
     while   True:
         print("\n")
         # Mostramos toda la info de la colección
         show_all_db_info(collection)
-        id = input("\nSeleccione el ID de la tarea que desea eliminar: ")
-        # Comprobamos
-        full_ids, _ = get_field_values(collection, 'Tarea') #Lista con los IDs de la colección
-        if  not id.isdigit() or not id in full_ids:
-            print(Back.RED + "El ID seleccionado no es válido. Inténtelo de nuevo." + Style.RESET_ALL)
-            continue
+        id = get_valid_input("\nSeleccione el ID de la tarea que desea eliminar: ",
+            lambda x: x.isdigit() and x in full_ids,
+            "El ID seleccionado no es válido. Inténtelo de nuevo.")
         break
 
         # Pedimos confirmación
@@ -197,7 +190,7 @@ def delete_task(collection):
     task = doc_ref.to_dict()["Tarea"]
     confirm = input("Esta seguro de querer eliminar la tarea: " + Fore.YELLOW + f"{task}" + Style.RESET_ALL + " ? (s/n): " )
     if confirm != 's':
-        print(Back.RED + "Eliminación cancelada." + Style.RESET_ALL)
+        print(Back.RED + "Eliminación cancelada.\n" + Style.RESET_ALL)
         return
     collection.document(id).delete()
     print(Fore.GREEN + "Tarea eliminada correctamente.\n" + Style.RESET_ALL)
