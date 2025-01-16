d3203aed057d31a4737377744387398357d636d4
arlo <arlotetxu@protonmail.com>
2025-01-16 16:35:11 +0100

    Starting new project: calculator with a GUI

---
+++ calculadora_gui/src/calculadora_gui.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+# Crear ventana principal
+root = tk.Tk()
+root.title("Calculadora Básica")
+
+# Establecer dimensiones de la ventana
+root.geometry("300x400")  # Ancho x Alto
+
+# Ejecutar el bucle principal de la interfaz gráfica
+root.mainloop()
